172b0b3300bfb79f27f87333ccb9122a847503ba
agysunny12 <119854958+agysunny12@users.noreply.github.com>
2022-12-12 23:56:44 +0530

    Update modelgeneration.py.py

--- Classes/modelgeneration.py.py
+++ Classes/modelgeneration.py.py
@@ -70,3 +70,50 @@ m.model_creation()
 gui=gui_model()
 
 print("finished")
+
+import __init__
+from __init__ import *
+class create_gui:
+    def __init__(self):
+        self.base=Tk()
+        self.main_window()
+    #run window
+    def run(self):
+        self.base.mainloop()
+    def main_window(self):
+        self.base.title("chat")
+        self.base.geometry("400x500")
+        self.base.resizable(width=FALSE, height=FALSE)
+        self.chat_window()
+    def send(self):
+        print("send")
+        msg = self.EntryBox.get("1.0",'end-1c').strip()
+        self.EntryBox.delete("0.0",END)
+        if msg != '':
+            self.ChatLog.config(state=NORMAL)
+            self.ChatLog.insert(END, "You: " + msg + '\n\n')
+            self.ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+            res = gui.chatbot_response(msg)
+            self.ChatLog.insert(END, "Bot: " + res + '\n\n')
+            self.ChatLog.config(state=DISABLED)
+            self.ChatLog.yview(END)
+    def chat_window(self):
+        print("create chat window")
+        self.ChatLog = Text(self.base, bd=0, bg="white", height="8", width="50", font="Arial",)
+        self.ChatLog.config(state=DISABLED)
+        #Bind scrollbar to Chat window
+        self.scrollbar = Scrollbar(self.base, command=self.ChatLog.yview, cursor="heart")
+        self.ChatLog['yscrollcommand'] = self.scrollbar.set
+        #create send button
+        self.SendButton = Button(self.base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
+                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+                    command= self.send)
+        print("going to entry box")
+        self.EntryBox = Text(self.base, bd=0, bg="white",width="29", height="5", font="Arial")    
+        self.scrollbar.place(x=376,y=6, height=386)
+        self.ChatLog.place(x=6,y=6, height=386, width=370)
+        self.EntryBox.place(x=128, y=401, height=90, width=265)
+        self.SendButton.place(x=6, y=401, height=90)
+        print("finish entry")
+        self.run()
+c_gui=create_gui()
